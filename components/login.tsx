7b2882752e138435caa1c801697bdae5eebe2e16
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 02:34:17 -0300

    add: connect wallet in login component

--- components/login.tsx
+++ components/login.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+declare global {
+    interface Window {
+        ethereum: any
+    }
+}
+
+function addrFormatter(s: string, size = 3) {
+    var first = s.slice(0, size + 1)
+    var last = s.slice(-size)
+    return `${first}_${last}`
+}
 
 export default function Login() {
+    const [wallet, setWallet] = useState('Login')
+    const connectWallet = async () => {
+        const listUserAddress = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+        })
+        console.log(listUserAddress)
+        setWallet(addrFormatter(listUserAddress[0]))
+        // setWallet("Logout")
+    }
+
     return (
-        <button className="login" onClick={() => console.log('clickou')}>
-            Login
+        <button className="login" onClick={connectWallet}>
+            {wallet}
         </button>
     )
 }
