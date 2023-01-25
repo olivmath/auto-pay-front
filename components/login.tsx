2c369c0a5079ea535fe5bf847b5a301e2bd4c56a
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 01:54:54 -0300

    add: login: component

---
+++ components/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <button className="login" onClick={() => console.log('clickou')}>
+            Login
+        </button>
+    )
+}
