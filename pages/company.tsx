11e5e1684254aecd160e389c1579e8a0d88138f4
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 15:37:41 -0300

    add: company page

---
+++ pages/company.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Company() {
+    return (
+        <>
+            <p>Company</p>
+            <Link href="/">{'back'}</Link>
+        </>
+    )
+}
