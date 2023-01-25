d3e3bfd442b969a2d020e6fd70efb776ad7479f4
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 01:55:38 -0300

    add: store front component

---
+++ components/storeFront.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StoreFront() {
+    return (
+        <>
+            <div className="store-front">
+                <div className="title">Auto Payments</div>
+            </div>
+        </>
+    )
+}
