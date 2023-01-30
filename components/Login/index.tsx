b57751cc07a2114974454d21fe0b63885722c3b7
Lucas Oliveira <olivmath@protonmail.com>
2023-01-30 01:33:51 -0300

    remove: unused code

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,48 +1,14 @@
-import { useRouter } from 'next/router'
 import React, { useState, createContext } from 'react'
-import { ethers } from 'ethers'
+import { useRouter } from 'next/router'
+import { getWallet } from './utils'
 
-interface AccountsPermission {
-    id: string
-    parentCapability: string
-    invoker: string
-    caveats: Array<{
-        type: string
-        value: string[]
-    }>
-    date: number
-}
-
-interface SignInData {
-    message: string
-    connected: boolean
-    address: string[]
-    className: string
-}
 declare global {
     interface Window {
         ethereum: any
     }
 }
 
-async function verifySignature(message: string, signature: string) {
-    const signer = await window.ethereum.request({
-        method: 'eth_accounts',
-    })
-    const verify = ethers.utils.verifyMessage(message.toString(), signature)
-    if (verify.toLowerCase() !== signer[0].toLowerCase()) {
-        alert('🚨 Signature not valid')
-    } else {
-        alert(`✅ connected: ${signer[0].toLowerCase()}`)
-    }
-}
-
 export const LoginContext = createContext({})
-
-function toWallet(acc: AccountsPermission[], wallet: SignInData): SignInData {
-    wallet.address = acc[0].caveats[0].value
-    return wallet
-}
 
 export function Login() {
     const router = useRouter()
@@ -54,25 +20,8 @@ export function Login() {
     })
 
     const connectWallet = async () => {
-        if (wallet.connected === false) {
-            const signIn: SignInData = toWallet(
-                await window.ethereum.request({
-                    method: 'wallet_requestPermissions',
-                    params: [{ eth_accounts: {} }],
-                }),
-                wallet
-            )
-            const response = await window.ethereum.request({
-                method: 'personal_sign',
-                params: [
-                    'Sign for Login',
-                    signIn.address[0],
-                    'Example password',
-                ],
-            })
-
-            await verifySignature('Sign for Login', response)
-
+        if (!wallet.connected) {
+            const signIn = await getWallet(wallet)
             setWallet({
                 message: 'Logout',
                 connected: true,
