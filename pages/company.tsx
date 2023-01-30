e94a2f44d7e9f30f993251865856566fb8c396fd
Lucas Oliveira <olivmath@protonmail.com>
2023-01-30 01:44:04 -0300

    add: content to company page

--- pages/company.tsx
+++ pages/company.tsx
@@ -3,7 +3,26 @@ import Link from 'next/link'
 export default function Company() {
     return (
         <>
-            <p>Company</p>
+            <p>Company name</p>
+            <p>Company Description</p>
+
+            <ul>
+                <li>
+                    <p>Employee 1</p>
+                    <p>100 ETH</p>
+                    <p>next payment: 12345</p>
+                </li>
+                <li>
+                    <p>Employee 2</p>
+                    <p>100 ETH</p>
+                    <p>next payment: 12345</p>
+                </li>
+                <li>
+                    <p>Employee 3</p>
+                    <p>100 ETH</p>
+                    <p>next payment: 12345</p>
+                </li>
+            </ul>
             <Link href="/">{'back'}</Link>
         </>
     )
